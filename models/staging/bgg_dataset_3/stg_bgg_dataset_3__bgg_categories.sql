7e227747d1c67784a1ee3c3813fde891b5b9ae72
Elise GR <elise.gr.data@gmail.com>
2025-12-02 17:21:25 +0000

    adding "categories" in column headers

--- models/staging/bgg_dataset_3/stg_bgg_dataset_3__bgg_categories.sql
+++ models/staging/bgg_dataset_3/stg_bgg_dataset_3__bgg_categories.sql
@@ -10,16 +10,16 @@ renamed as (
 
     select
         bggid as id,
-        exploration,
-        miniatures,
-        territory_building,
-        card_game,
-        educational,
-        puzzle,
-        collectible_components,
-        word_game,
-        print___play,
-        electronic
+        exploration as categories_exploration,
+        miniatures as categories_miniatures,
+        territory_building as categories_territory_building,
+        card_game as categories_card_game,
+        educational as categories_educational,
+        puzzle as categories_puzzle,
+        collectible_components as categories_collectible_components,
+        word_game as categories_word_game,
+        print___play as categories_print_play,
+        electronic as categories_electronic
 
     from source
 
