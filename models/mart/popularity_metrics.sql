986eb0254b6790498b6e2ee87a992d8084c44733
Quentin Carreau <quentincarreau@gmail.com>
2025-12-09 17:15:55 +0000

    Add aggregation

--- models/mart/popularity_metrics.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    safe_divide(nb_wanting, nb_owned) as interest_to_ownership, 
-  FROM {{ ref('stg_bgg_dataset_2__games_detailed_info2025') }} as gdi
-  INNER JOIN {{ ref('master_bgg') }} as dbt
-  using(id)
-  order by popularity_score desc
